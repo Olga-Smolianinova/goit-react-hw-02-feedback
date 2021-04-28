1d5b32f8e3dad6e1c37d6a43f39177aa91094bd8
Olga-Smolianinova <olga.smol2015@gmail.com>
2021-04-28 17:20:37 +0300

    delete feedback file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,91 @@
 import React, { Component } from 'react';
 
 // Components
-import Feedback from './components/Feedback';
-
-// import Draft from './components/Draft'; // для тренировки теории модуля
+// import Feedback from './components/Feedback';
+import Section from './components/Section';
+import FeedbackOptions from './components/FeedbackOptions';
+import Statistics from './components/Statistics';
+import Notification from './components/Notification';
 
 // Styles
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        {/* Home work */}
-        <Feedback />
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-        {/* для тренировки теории по модулю components//Draft/Draft.js
-        <Draft /> */}
+  // методы
+  countFeedback = event => {
+    const btnName = event.target.textContent.toLowerCase();
+
+    this.setState(prevState => {
+      // перебираем this.state. Если в переборе ключ и имя кнопки (приведенное к нижнему регистру) совпадает, то значение ключа +1
+      for (const key in prevState) {
+        if (key === btnName) {
+          return { [key]: prevState[key] + 1 };
+        }
+      }
+      // вариант 2 - тоже самое через распыление
+      // ...prevState,
+      // ...{
+      //   [btnName]:
+      //     prevState[btnName] + 1,
+      // },
+    });
+  };
+
+  // метод для расчета общего количества отзывов (total). Для использования этого способа в render() прописываем totalFeedback=this.countTotalFeedback()
+
+  // countTotalFeedback = () => {
+  //   const values = Object.values(this.state);
+  //   console.log(values);
+  //   const totalFeedback = values.reduce((acc, value) => acc + value, 0);
+  //   return totalFeedback;
+  // };
+
+  // render
+  render() {
+    // для оптимизации кода деструктуризируем свойства state,чтобы каждый раз не писать this.state в Statistics
+    const { good, neutral, bad } = this.state;
+    // console.log(this.state);
+
+    // для расчета общего количества отзывов (total)
+    const countTotalFeedback = good + neutral + bad;
+
+    // для расчета % положительных отзывов
+    const countPositiveFeedbackPercentage = Math.round(
+      (good * 100) / countTotalFeedback,
+    );
+
+    return (
+      <div className="Feedback">
+        {/*1. title выносим в отдельный component 
+        /* 2.  внутрь Section рендерим и вставляем часть кода из ./FeedbackOptions.js */}
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.countFeedback}
+          />
+        </Section>
+
+        {/*3. title выносим в отдельный component
+        4. внутрь Section рендерим и вставляем часть кода из ./Statistics.js */}
+        <Section title="Statistics">
+          {/* рендер по условию: если есть хотя бы 1 отзыв выведи всю статистику, в обратном случае - надпись No feedback given' */}
+          {countTotalFeedback !== 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback}
+              positivePercentage={countPositiveFeedbackPercentage}
+            />
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
       </div>
     );
   }
